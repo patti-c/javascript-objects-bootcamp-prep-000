ac8ce7ac34cc669715d32a67f961c284a9988eb7
Patricia Carter <patricia4194@gmail.com>
2018-07-12 17:42:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,26 @@
 var playlist = {
+<<<<<<< HEAD
   gaga: "Bad Romance"
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   console.log(playlist)
+=======
+  bowie: "Space Odyssey"
+}
+
+function updatePlaylist(playlist, artist, title) {
+  playlist.artist = title
+>>>>>>> a3affffadc261b4a69377f730a82160f2259d23f
   return playlist
 }
 
 function removeFromPlaylist(playlist, artist) {
+<<<<<<< HEAD
   delete playlist[artist]
+=======
+  delete playlist.artist
+>>>>>>> a3affffadc261b4a69377f730a82160f2259d23f
   return playlist
 }
